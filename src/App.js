39ace78c8adbd6a95c3a99b2930d8939ba7eb0d1
omerSut <omer.sut@santsg.com>
2022-09-15 15:50:24 +0300

    TEST

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import Login from './components/login';
-import Logout from './components/logout';
-import './App.css';
-import { useSelector} from 'react-redux';
-import { selectUser } from './features/userSlice';
+import Login from "./components/login";
+import Logout from "./components/logout";
+import "./App.css";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 
 function App() {
   const user = useSelector(selectUser);
-  return (
-    <div className="App">
-      {user ? <Logout/>: <Login/>}
-   </div>
-  );
+  return <div className="App">{user.loggedIn ? <Logout /> : <Login />}</div>;
 }
 
 export default App;
--- src/features/userSlice.jsx
+++ src/features/userSlice.jsx
@@ -3,18 +3,27 @@ import {createSlice} from '@reduxjs/toolkit';
 export const userSlice = createSlice({
     name: 'user',
     initialState: {
-        user: null
-    },
-    reducers: {
+        name: null,
+        email: null,
+        password: null,
+        loggedIn: false,
+      },
+      reducers: {
         login: (state, action) => {
-            state.user = action.payload;
+          state.name = action.payload.name;
+          state.email = action.payload.email;
+          state.password = action.payload.password;
+          state.loggedIn = action.payload.loggedIn;
         },
         logout: (state, action) => {
-            state.user = null;
-        }
+          state.name = null;
+          state.email = null;
+          state.password = null;
+          state.loggedIn = false;
+        },
 
     }
 });
 export const {login, logout} = userSlice.actions;
-export const selectUser = state => state.user.user;
+export const selectUser = state => state.user;
 export default userSlice.reducer;
